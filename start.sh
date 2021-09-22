b7dfa39c5b7e3c9f33cbe3da5602fb4cb271551e
ChisBread <chisbread@gamil.com>
2021-09-22 13:39:18 +0800

    fix ca && timezone

--- start.sh
+++ start.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 if [ ! -x "/config/settings.json" ]; then
-    _=`mkdir /config`
     cp /settings.json /config/settings.json
 fi
 transmission-daemon -g /config -e /config/transmission.log -x /var/transmission.pid
 sleep 2
-tail --pid=`cat /var/transmission.pid` -f /dev/null
+tail -f /config/transmission.log
