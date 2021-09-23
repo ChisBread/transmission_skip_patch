38e2b0bd1d3ae8fdcce2e4484cd730b8487359e3
ChisBread <chisbread@gamil.com>
2021-09-23 14:14:45 +0800

    简单粗暴

--- patchspk.sh
+++ patchspk.sh
@@ -31,7 +31,7 @@ fi
 mkdir -p /tmp/transmission_skip_patch
 cd /tmp/transmission_skip_patch
 wget ${SPK_D} -O tr.spk && tar xf tr.spk && tar xf package.tgz
-chown sc-transmission:transmission bin/transmission-daemon && chmod 765 bin/transmission-daemon
+chown sc-transmission:transmission bin/transmission-daemon && chmod 777 bin/transmission-daemon
 
 mv ${TRROOT}/bin/transmission-daemon ${TRROOT}/bin/transmission-daemon.bak
 echo '备份至:'${TRROOT}'/bin/transmission-daemon.bak'
