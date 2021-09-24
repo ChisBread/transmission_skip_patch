8f6ee7a289375800921d5b14d0f56d5402139662
ChisBread <chisbread@gmail.com>
2021-09-24 13:43:51 +0800

    download archive

--- patchspk.sh
+++ patchspk.sh
@@ -23,14 +23,15 @@ SPK_D=''
 _=`uname -a |grep '4.4.59+'`
 if [ $? -eq 0 ]; then
     echo '系统为DSM6.x'
-    SPK_D='https://github.com/ChisBread/transmission_skip_patch/raw/master/spks/transmission_x64-6.2.3_3.00-19.spk'  
+    SPK_D='transmission_skip_patch-master/spks/transmission_x64-6.2.3_3.00-19.spk'  
 else
     echo '系统为DSM7.0'
-    SPK_D='https://github.com/ChisBread/transmission_skip_patch/raw/master/spks/transmission_x64-7.0_3.00-19.spk'
+    SPK_D='transmission_skip_patch-master/spks/transmission_x64-7.0_3.00-19.spk'
 fi
 mkdir -p /tmp/transmission_skip_patch
 cd /tmp/transmission_skip_patch
-wget ${SPK_D} -O tr.spk && tar xf tr.spk && tar xf package.tgz
+wget https://github.com/ChisBread/transmission_skip_patch/archive/refs/heads/master.tar.gz --no-check-certificate && tar zxf master.tar.gz
+mv ${SPK_D} tr.spk && tar xf tr.spk && tar xf package.tgz
 if [ ! -x "bin/transmission-daemon" ]; then
     echo 'bin/transmission-daemon 不存在,请检查网络是否能访问github'
     exit 1
