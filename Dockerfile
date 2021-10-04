24cebc84eeae8beb74cb181ed605aadea8e8344e
ChisBread <chisbread@gmail.com>
2021-10-05 00:16:12 +0800

    优雅处理退出

--- Dockerfile
+++ Dockerfile
@@ -7,20 +7,24 @@ RUN apt-get update; \
     rm -rf /var/lib/apt/lists/*
 # 拷贝需要的文件
 COPY spks/transmission_x64-6.2.3_3.00-19.spk transmission_x64-6.2.3_3.00-19.spk
-COPY install_webui.sh install_webui.sh
-COPY start.sh /start.sh
-COPY settings.json /settings.json
 ENV TRANSMISSION_WEB_HOME=/usr/share/transmission/web \
     TZ=Asia/Shanghai 
 # 安装transmission
 RUN mkdir -p /tmp/tr && cd /tmp/tr && cp /transmission_x64-6.2.3_3.00-19.spk . && \
     tar xvf transmission_x64-6.2.3_3.00-19.spk && tar xvf package.tgz && \
     chown -R root:root * && chmod 765 lib/* bin/* && cp bin/* /usr/bin/ && cp -r lib/* /lib/x86_64-linux-gnu/ && \
-    cp -r share/transmission /usr/share
+    cp -r share/transmission /usr/share && cd - && rm -rf /tmp/tr
 # 安装增强UI
+COPY install_webui.sh install_webui.sh
 RUN bash install_webui.sh
+COPY start.sh /start.sh
+COPY settings.json /settings.json
 # ports and volumes
-RUN chmod 765 /start.sh
-EXPOSE 9091 51413/tcp 51413/udp
+RUN chmod +x /start.sh
+ENV USER='transmission'
+ENV PASS='transmission'
+ENV RPCPORT=9091
+ENV PEERPORT=51413
+EXPOSE $PORT $PEERPORT/tcp $PEERPORT/udp
 VOLUME /config /downloads /watch
-CMD [ "/start.sh" ]
+ENTRYPOINT [ "/start.sh" ]
--- start.sh
+++ start.sh
@@ -1,11 +1,43 @@
 #!/bin/bash
-if [ ! -x "/config/settings.json" ]; then
+#清理
+_term() {
+  echo "Caught SIGTERM signal!"
+  echo "Tell the transmission session to shut down."
+  pid=`cat /var/transmission.pid`
+  if [ ! -z "$USER" ] && [ ! -z "$PASS" ]; then
+    /usr/bin/transmission-remote $RPCPORT -n "$USER":"$PASS" --exit
+  else
+    /usr/bin/transmission-remote $RPCPORT --exit
+  fi
+  # terminate when the transmission-daemon process dies
+  tail --pid=${pid} -f /dev/null
+}
+trap _term SIGTERM
+#初始化
+if [ ! -f "/config/settings.json" ]; then
     cp /settings.json /config/settings.json
-    chmod 777 /config/settings.json
 fi
-if [ ! -x "/usr/share/transmission/web/index.html" ]; then
+if [ ! -f "/usr/share/transmission/web/index.html" ]; then
     mv /usr/share/transmission/web/transmission-web-control-master/src/* /usr/share/transmission/web/
 fi
-transmission-daemon -g /config -e /config/transmission.log -x /var/transmission.pid
-sleep 2
-tail -f /config/transmission.log
+#用户
+if [ ! -z "$USER" ] && [ ! -z "$PASS" ]; then
+	sed -i '/rpc-authentication-required/c\    "rpc-authentication-required": true,' /config/settings.json
+	sed -i "/rpc-username/c\    \"rpc-username\": \"$USER\"," /config/settings.json
+	sed -i "/rpc-password/c\    \"rpc-password\": \"$PASS\"," /config/settings.json
+else
+	sed -i '/rpc-authentication-required/c\    "rpc-authentication-required": false,' /config/settings.json
+	sed -i "/rpc-username/c\    \"rpc-username\": \"$USER\"," /config/settings.json
+	sed -i "/rpc-password/c\    \"rpc-password\": \"$PASS\"," /config/settings.json
+fi
+#端口
+if [ ! -z "$RPCPORT" ]; then
+	sed -i "/rpc-port/c\    \"rpc-port\": $RPCPORT," /config/settings.json
+fi
+if [ ! -z "$PEERPORT" ] ; then
+	sed -i "/peer-port/c\    \"peer-port\": $PEERPORT," /config/settings.json
+fi
+: >/config/transmission.log
+tail -f /config/transmission.log &
+transmission-daemon -g /config -c /watch -e /config/transmission.log -x /var/transmission.pid -f &
+wait
