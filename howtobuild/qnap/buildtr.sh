acfeb20621189e524024c61b7a4dbe6ec6e53a50
Bread <chisbread@gmail.com>
2022-01-01 15:49:46 +0800

    Update buildtr.sh

--- howtobuild/qnap/buildtr.sh
+++ howtobuild/qnap/buildtr.sh
@@ -1,3 +1,2 @@
 #cd ./TransmissionQPKG && docker run -it --rm -v ${PWD}:/my_project mrmoneyc/docker-qdk bash -c "cd /my_project; qbuild"
 docker run -it --rm -v ${PWD}/TransmissionQPKG:/src dorowu/qdk2-build bash -c "cd /src && /usr/share/qdk2/QDK/bin/qbuild"
-cp TransmissionQPKG/build/* /mnt/planet/data/TransmissionQPKG/build/
